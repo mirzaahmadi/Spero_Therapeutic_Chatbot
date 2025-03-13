8ad3f104dd4b0ae79fa04d8b963476d45bd33956
“mirzaahmadi” <mamahmadi727@gmail.com>
2025-03-13 17:31:05 -0400

    updates to preprocessing

---
+++ Preprocessing.py
@@ -0,0 +1,18 @@
+# Import necessary libraries for preprocessing
+import pandas as pd
+
+# Read in the training dataset
+def main(df):
+    unprocessed_df = pd.read_csv(df)
+    preprocess(unprocessed_df)
+
+# Go through the preprocessing steps: Lowercasing, Removing punctuation, Removing stopwords
+def preprocess(dataframe):
+    dataframe["conversations"] = dataframe["conversations"].str.lower()
+
+    
+
+
+if __name__ == "__main__":
+    df = input("Insert Dataset Name: ")
+    main(df)
